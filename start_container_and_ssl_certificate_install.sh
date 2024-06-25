ebe21034f5ebbb5028b7b8c2db98ba58c0614c21
Forward489 <glennstevensantoso@gmail.com>
2024-06-25 12:08:29 +0700

    feat: modify files

--- start_container_and_ssl_certificate_install.sh
+++ start_container_and_ssl_certificate_install.sh
@@ -15,7 +15,7 @@ sudo apt install apache2 -y
 sudo a2enmod proxy proxy_http ssl headers
 sudo systemctl restart apache2
 
-cat <<EOF > /etc/apache2/sites-available/wordpress.conf
+sudo cat <<EOF > /etc/apache2/sites-available/wordpress.conf
 <VirtualHost *:80>
     ServerName $domain
 
@@ -24,16 +24,16 @@ cat <<EOF > /etc/apache2/sites-available/wordpress.conf
     ProxyPassReverse / http://localhost:8080/
     
     RewriteEngine on
-    RewriteCond %{SERVER_NAME} =docker.mapitout.site
-    RewriteRule ^ https://%{SERVER_NAME}%{REQUEST_URI} [END,NE,R=permanent]
+    # RewriteCond %{SERVER_NAME}=$domain
+    RewriteRule ^ https://$domain%{REQUEST_URI} [END,NE,R=permanent]
 </VirtualHost>
 
 <VirtualHost *:443>
     ServerName $domain
 
     SSLEngine on
-    SSLCertificateFile /etc/letsencrypt/live/docker.mapitout.site/fullchain.pem
-    SSLCertificateKeyFile /etc/letsencrypt/live/docker.mapitout.site/privkey.pem
+    SSLCertificateFile /etc/letsencrypt/live/$domain/fullchain.pem
+    SSLCertificateKeyFile /etc/letsencrypt/live/$domain/privkey.pem
     Include /etc/letsencrypt/options-ssl-apache.conf
 
     ProxyPreserveHost On
